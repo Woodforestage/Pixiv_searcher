f444aca9ef15e55a0e423798399eb1fd8a97a9d2
Woodforestage <kn16sub@gmail.com>
2025-06-05 23:44:33 +0900

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session, flash
 import db  # DB操作用のモジュール（先のsqlite管理関数群）
 import os
-from functionsp import save_cookies as scs, search_and_graph as sag, is_cookie_valid
+from functions import save_cookies as scs, search_and_graph as sag, is_cookie_valid
 
 #db初期化
 db.init_db()
